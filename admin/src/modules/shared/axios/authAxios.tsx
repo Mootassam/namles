7cfce114d7d147a30f4214382556ca4f4261dd0f
Mootassam <mootassame@gmail.com>
2024-10-31 07:29:27 -0400

    add the new commit and change the url

--- admin/src/modules/shared/axios/authAxios.tsx
+++ admin/src/modules/shared/axios/authAxios.tsx
@@ -10,7 +10,7 @@ const authAxios = Axios.create({
   // baseURL: "http://192.168.1.43:8080/api",
 
   //Namles
-  baseURL: 'http://139.162.184.226:8080/api/',
+  baseURL: 'http://162.0.222.192:8088/api/',
 
   paramsSerializer: function (params) {
     return Qs.stringify(params, {
--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
-  baseURL: 'http://139.162.184.226:8080/api/',
-  // baseURL: 'http://192.168.1.43:8080/api/',
+  // baseURL: 'http://139.162.184.226:8080/api/',
+  baseURL: 'http://162.0.222.192:8088/api/',
 });
 
 authAxios.interceptors.request.use(async function (options) {
