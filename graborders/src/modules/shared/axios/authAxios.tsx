2dce80a2d40efb57e0fe01e967f71b8da7cdc1ff
Mootassam <mootassame@gmail.com>
2024-10-31 11:33:23 -0400

    add the new namles

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -3,7 +3,7 @@ import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
   // baseURL: 'http://139.162.184.226:8080/api/',
-  baseURL: 'http://162.0.222.192:8088/api/',
+  baseURL: 'http://209.74.95.197:8088/api/',
 });
 
 authAxios.interceptors.request.use(async function (options) {
